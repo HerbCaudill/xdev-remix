c8a4dd29e1d938bb81e1ff3be689a1368d351223
Herb Caudill <herb@devresults.com>
2024-03-03 13:27:40 +0100

    bump lf/auth deps, use @herbcaudill/eventemitter

--- app/lib/eventPromise.ts
+++
@@ -1,9 +0,0 @@
-export const eventPromise = async (emitter: EventEmitter, event: string) =>
-  new Promise<any>(resolve => {
-    emitter.once(event, (d: any) => resolve(d))
-  })
-
-// stub this out rather than importing the package
-type EventEmitter = {
-  once(event: string, listener: (...args: any[]) => void): EventEmitter
-}
